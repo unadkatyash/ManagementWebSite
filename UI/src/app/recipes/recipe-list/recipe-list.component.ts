e20719eb7d72277e8f672dcdbb7192111af92312
Yash Unadakt <yash.unadkat@tatvasoft.com>
2024-11-07 19:01:35 +0530

    054 Adding Content to the Recipes Components

--- UI/src/app/recipes/recipe-list/recipe-list.component.ts
+++ UI/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeItemComponent } from "./recipe-item/recipe-item.component";
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [RecipeItemComponent],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes : Recipe[] = [new Recipe('A Test Recipe', 'This is simply a test','https://i0.wp.com/picjumbo.com/wp-content/uploads/korean-bibimbap-flatlay.jpg?w=600&quality=80')]
 
 }
