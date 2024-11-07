09c69af273abca57eb879ec038bc178ed7fa376d
Yash Unadakt <yash.unadkat@tatvasoft.com>
2024-11-07 16:45:15 +0530

    049 Creating the Components

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailsComponent } from "./recipes/recipe-details/recipe-details.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailsComponent, ShoppingEditComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'UI';
 }
