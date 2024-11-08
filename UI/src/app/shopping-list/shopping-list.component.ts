5370e266f90c1a8177d0b5828a23d93f5a8ecb41
Yash Unadakt <yash.unadkat@tatvasoft.com>
2024-11-08 12:08:36 +0530

    059 Creating and Outputting the Shopping List

--- UI/src/app/shopping-list/shopping-list.component.ts
+++ UI/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { CommonModule } from '@angular/common';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [ShoppingEditComponent],
+  imports: [ShoppingEditComponent, CommonModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomato', 10),
+  ];
   constructor() {}
   ngOnInit() {}
 }
